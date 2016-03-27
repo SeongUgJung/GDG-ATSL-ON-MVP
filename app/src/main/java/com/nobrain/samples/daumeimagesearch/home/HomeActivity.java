d983412237da3c93f62b643f588cbc2fcc14e779
jsuch2362 <jsuch2362@gmail.com>
2016-03-27 15:12:13 +0900

    init library

---
+++ app/src/main/java/com/nobrain/samples/daumeimagesearch/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.nobrain.samples.daumeimagesearch.home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nobrain.samples.daumeimagesearch.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
